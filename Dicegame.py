23a0f2e4f47d6c2e66a8a8bfc0c5dac2edd22555
kage-dot <s1320139@u-aizu.ac.jp>
2025-06-27 19:12:19 +0900

    Dice game: initial revision

---
+++ Dicegame.py
@@ -0,0 +1,12 @@
+import random
+
+print("Rolling the dice...")
+
+die1 = random.randint(1,6)
+die2 = random.randint(1,6)
+
+print("Die 1: " + str(die1))
+print("Die 2: " + str(die2))
+
+total = die1 + die2
+print("Total value: " + str(total))
